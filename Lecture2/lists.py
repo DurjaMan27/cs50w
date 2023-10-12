ed444941f44bcf4b5da98a706527439bdf53fc14
Varun Rao <varunvellorerao@gmail.com>
2023-10-12 16:12:40 -0400

    Going over list functions

---
+++ Lecture2/lists.py
@@ -0,0 +1,8 @@
+# creates and defines a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+print(names)
+
+# modifying names within the lists
+names.append("Draco") # adds Draco to the beginning of the list
+names.sort() # sorts everything in the list
+print(names)
