614df81ca4b0cbf72e9b5e045985fedb1d112a13
Varun Rao <varunvellorerao@gmail.com>
2023-10-12 16:13:16 -0400

    Reviewing loops

---
+++ Lecture2/loops.py
@@ -0,0 +1,12 @@
+# creating a loop to print numbers 0-5
+for i in range(6):
+    print(i)
+
+names = ["Harry", "Ron", "Hermione"]
+for name in names:
+    print(name)
+
+
+name = "Harry"
+for char in name:
+    print(char)
