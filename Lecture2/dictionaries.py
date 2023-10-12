46aa99d5a17331a7031f6d6f1b44b63c6b12163d
Varun Rao <varunvellorerao@gmail.com>
2023-10-12 16:13:28 -0400

    Reviewing dictionaries

---
+++ Lecture2/dictionaries.py
@@ -0,0 +1,8 @@
+# creating a dictionary with key-value pairs
+houses = {"Harry":"Gryffindor", "Draco":"Slytherin"}
+
+print(houses["Harry"]) # allows you to look up a key to print the value
+
+# adding values to dict
+houses["Hermione"] = "Gryffindor"
+print(houses)
