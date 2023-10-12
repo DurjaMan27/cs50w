c1cc9d481e19ea0557ea413182d623e88445b345
Varun Rao <varunvellorerao@gmail.com>
2023-10-12 16:12:25 -0400

    Reviewing lists and tuples

---
+++ Lecture2/sequences.py
@@ -0,0 +1,7 @@
+name = "Harry"
+print(name[0]) # prints the first character of the variable name
+
+names = ["Harry", "Ron", "Hermione"] # list
+print(names[2])
+
+coordinate = (10.0, 20.0) # tuple - used when you have a pair of values that aren't going to change
