316cfb0c962087d957df6c59a3fb80624b7f0627
Varun Rao <varunvellorerao@gmail.com>
2023-10-21 13:18:59 -0400

    Introduce interval and localStorage

--- Lecture5/counter.js
+++ Lecture5/counter.js
@@ -1,14 +1,15 @@
-let counter = 0; // defines new variable
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
     counter++;
     document.querySelector('h1').innerHTML = counter;
-
-    if (counter % 10 === 0) {
-        alert(`Count is now ${counter}`);
-    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = count;
+
+    // runs the count function every 1000 milliseconds (1 second)
+    setInterval(count, 1000);
 });
