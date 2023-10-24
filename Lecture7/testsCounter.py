debf848c3da6e1d959d8f2996f3d9596174ca060
Varun Rao <varunvellorerao@gmail.com>
2023-10-24 00:20:52 -0400

    Adding further tests to the counter.html file

--- Lecture7/testsCounter.py
+++ Lecture7/testsCounter.py
@@ -10,3 +10,27 @@ def file_uri(filename):
 
 driver = webdriver.Chrome()
 
+class WebpageTests(unittest.TestCase):
+
+    def test_title(self):
+        driver.get(file_uri("counter.html"))
+        self.assertEqual(driver.title, "Counter")
+
+    def test_increase(self):
+        driver.get(file_uri("counter.html"))
+        increase = driver.find_element_by_id("increase")
+        increase.click()
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "1")
+
+    def test_increase(self):
+        driver.get(file_uri("counter.html"))
+        decrease = driver.find_element_by_id("decrease")
+        decrease.click()
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "-1")
+
+    def test_multiple_increase(self):
+        driver.get(file_uri("counter.html"))
+        increase = driver.find_element_by_id("increase")
+        for i in range(3):
+            increase.click()
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "3")
