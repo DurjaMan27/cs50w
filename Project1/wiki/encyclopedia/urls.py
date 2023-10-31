d7ec6a8d2b2dd2a7bdba98ae84ab577ec9b38288
Varun Rao <varunvellorerao@gmail.com>
2023-10-31 13:06:52 -0400

    Accidentally deleted stuff and returned it

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.title, name="title"),
+    path("wiki/<str:title>", views.title, name="title"),
     path("newentry", views.newEntry, name="newentry")
 ]
