87868380a1714c829604b18389215ee1ad0bfa0d
Varun Rao <varunvellorerao@gmail.com>
2023-10-31 10:42:42 -0400

    Using f-string to allow for multiple title urls

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -7,3 +7,24 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def title(request, title):
+    return render(request, f"encyclopedia/{title}.html", {
+        "title": util.get_entry(title)
+    })
+
+def newEntry(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "encyclopedia/add.html", {
+                "form": form
+            })
+
+    return render(request, "encyclopedia/add.html", {
+        "form": NewTaskForm()
+    })
