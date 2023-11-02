cdc760c2edb4b52b8bc5cefa16b4684185f2f188
Varun Rao <varunvellorerao@gmail.com>
2023-11-02 18:37:18 -0400

    Updated urls

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.title, name="title"),
+    path("error", views.error, name="error")
     #path("newentry", views.newEntry, name="newentry")
 ]
