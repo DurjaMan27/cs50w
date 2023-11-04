0ca5c6dc55e05b60a3e420c59f41cafd55c3494c
Varun Rao <varunvellorerao@gmail.com>
2023-11-04 11:52:45 -0400

    Created new class to edit entry, not create new

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -7,7 +7,10 @@ from django.http import HttpResponseRedirect
 from . import util
 
 class NewTaskForm(forms.Form):
-    title = forms.CharField(label="Wiki Page Title", max_length=20)
+    title = forms.CharField(label="Wiki Page Title", max_length=20, )
+    content = forms.CharField(label="content", widget=forms.Textarea(), max_length=400)
+
+class EditTaskForm(forms.Form):
     content = forms.CharField(label="content", widget=forms.Textarea(), max_length=400)
 
 def index(request):
@@ -33,6 +36,32 @@ def searchError(request):
 
 
 def newEntry(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        #variableTitle = form.cleaned_data["title"]
+        #variableTitle = form.title
+        #print("this is title ", variableTitle)
+        if form.is_valid():
+            #task = form.cleaned_data["task"]
+            #request.session["tasks"] += [task]
+            if util.get_entry(form.cleaned_data["title"]) != None:
+                print("i am here...")
+                return render(request, "encyclopedia/newEntryError.html", {
+                    "title": form.cleaned_data["title"]
+                })
+            else:
+                util.save_entry(form.cleaned_data["title"], form.cleaned_data["content"])
+                return HttpResponseRedirect(reverse("title", kwargs={'title': form.cleaned_data["title"]}))
+        else:
+            return render(request, "encyclopedia/newentry.html", {
+                "form": form
+            })
+
+    return render(request, "encyclopedia/newentry.html", {
+        "form": NewTaskForm()
+    })
+
+def editEntry(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         #variableTitle = form.cleaned_data["title"]
