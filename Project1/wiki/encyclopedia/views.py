3edcdb01a637276447a7cc203bc2aa6aa93d1332
Varun Rao <varunvellorerao@gmail.com>
2023-11-04 10:30:29 -0400

    Updating the links for the methods of new entry

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -36,18 +36,19 @@ def newEntry(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if util.get_entry(form.title) != None:
-            return render(request, "encyclopedia/error.html", {
+            return render(request, "encyclopedia/newEntryError.html", {
             "title": title.capitalize()
         })
         if form.is_valid():
             #task = form.cleaned_data["task"]
             #request.session["tasks"] += [task]
+            util.save_entry(form.title, form.content)
             return HttpResponseRedirect(reverse("title"))
         else:
-            return render(request, "encyclopedia/newEntry.html", {
+            return render(request, "encyclopedia/newentry.html", {
                 "form": form
             })
 
-    return render(request, "encyclopedia/newEntry.html", {
+    return render(request, "encyclopedia/newentry.html", {
         "form": NewTaskForm()
     })
