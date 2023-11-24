69203f7c4ae557bc58fc41d6fce6fc867a8bc2a4
Varun Rao <varunvellorerao@gmail.com>
2023-11-24 12:23:04 -0500

    Created new paths for showing single listing

--- Project2/commerce/auctions/urls.py
+++ Project2/commerce/auctions/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("newlisting", views.newlisting, name="newlisting")
+    path("newlisting", views.newlisting, name="newlisting"),
+    path("all_listings", views.all_listings, name="all_listings"),
+    path("<str:username>/<str:product>", views.listing, name="listing")
 ]
