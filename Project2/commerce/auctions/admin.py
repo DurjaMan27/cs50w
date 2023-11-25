dcbd2c1c504c55a475fb7893018a837fdde4aa5b
Varun Rao <varunvellorerao@gmail.com>
2023-11-25 17:40:02 -0500

    Adding all registered admin sites

--- Project2/commerce/auctions/admin.py
+++ Project2/commerce/auctions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Listing, Bid, Comment
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("auction_open", "user", "product_title", "product_description", "product_startingBid", "product_currentBidder",
+                    "product_category", "image_url")
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("user", "bid_amount", "product_title", "product_poster")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "comment", "product_title", "product_description")
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
