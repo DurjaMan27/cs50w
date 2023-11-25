a2dcc4d17203b9cb2d305611b311d8f8d28fcac8
Varun Rao <varunvellorerao@gmail.com>
2023-11-25 17:39:49 -0500

    Adding that User watchlist can be blank

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    watchlist = models.ManyToManyField('Listing', related_name='watchlist')
+    watchlist = models.ManyToManyField('Listing', related_name='watchlist', blank=True)
 
 class Listing(models.Model):
     auction_open = models.BooleanField(default=True)
