b113c7f9095156d488e63d8df415f6367b79dc85
Varun Rao <varunvellorerao@gmail.com>
2023-11-25 17:40:27 -0500

    Created path to close the listing

--- Project2/commerce/auctions/urls.py
+++ Project2/commerce/auctions/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("newlisting", views.newlisting, name="newlisting"),
     path("categories", views.categories, name="categories"),
     path("<str:category>", views.all_listings, name="all_listings"),
+    path("closeAuction", views.close_auction, name="close_auction"),
     path("<str:username>/<str:product>", views.listing, name="listing")
 ]
