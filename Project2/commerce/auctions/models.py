fc142d6070a78c7ce96662ac31920469e382feda
Varun Rao <varunvellorerao@gmail.com>
2023-11-30 22:51:41 -0500

    Still trying to figure out related_name

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    watchlist = models.ManyToManyField('Listing', blank=True)
+    watchlist = models.ManyToManyField('Listing', related_name="watchlist", blank=True)
     pass
 
 class Listing(models.Model):
