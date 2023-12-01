630fc0e1a1b53fdb2e20a39e499a2e9848588b29
Varun Rao <varunvellorerao@gmail.com>
2023-11-30 22:52:40 -0500

    Fixed some errors of fields that had been removed

--- Project2/commerce/auctions/admin.py
+++ Project2/commerce/auctions/admin.py
@@ -5,10 +5,10 @@ class ListingAdmin(admin.ModelAdmin):
     list_display = ("auction_open", "user", "product_title", "product_description", "product_startingBid", "product_currentBidder",
                     "product_category", "image_url")
 class BidAdmin(admin.ModelAdmin):
-    list_display = ("user", "bid_amount", "product", "product_poster")
+    list_display = ("user", "bid_amount", "product")
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("user", "comment", "product", "product_poster")
+    list_display = ("user", "comment", "product")
 
 # Register your models here.
 admin.site.register(User)
