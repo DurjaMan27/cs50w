5405c3b89136a55790dff18a623a090c55bde283
Varun Rao <varunvellorerao@gmail.com>
2023-11-30 22:44:02 -0500

    Removed all related names

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
-    watchlist = models.ManyToManyField('Listing', related_name='watchlist', blank=True)
+    watchlist = models.ManyToManyField('Listing', blank=True)
+    pass
 
 class Listing(models.Model):
     auction_open = models.BooleanField(default=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     product_title = models.CharField(max_length=64)
     product_description = models.CharField(max_length=255)
     product_startingBid = models.IntegerField()
-    product_currentBidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids', blank=True, null=True)
+    product_currentBidder = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     product_category = models.CharField(max_length=64, blank=True)
     image_url = models.CharField(max_length=255, blank=True)
 
