83efa395848718a18d1ab948a03cb947ffba4a51
Varun Rao <varunvellorerao@gmail.com>
2023-12-24 11:58:44 -0500

    Added a toString that doesn't work

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -17,6 +17,9 @@ class Listing(models.Model):
     product_category = models.CharField(max_length=64, blank=True)
     image_url = models.CharField(max_length=255, blank=True)
 
+    def __str__(self):
+        return "Auction Status: " + str(self.auction_open) + "\nID: " + self.listing_id + "\nUser: " + self.user + "\nTitle: " + self.product_title + "\nDescription" + self.product_description + "\nBid: " + self.product_startingBid + "\nBidder: " + self.product_currentBidder + "\nCategory: " + self.product_category + "\nimage: " + self.image_url
+
 class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     bid_amount = models.IntegerField()
