e325adbc43eaabc0a8f58264b79e3b8f7ac3ef13
Varun Rao <varunvellorerao@gmail.com>
2024-02-03 14:36:18 -0500

    Basic fields for models

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -3,4 +3,33 @@ from django.db import models
 
 
 class User(AbstractUser):
+    watchList = models.ManyToManyField("Listing", related_name="listings")
     pass
+
+class Listing(models.Model):
+    CATEGORIES = {
+        "Fashion": "Fashion",
+        "Toys": "Toys",
+        "Electronics": "Electronics",
+        "Home": "Home",
+        "General": "General"
+    }
+    listingID = models.AutoField(primary_key=True)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    datePosted = models.DateTimeField()
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=255)
+    startingBid = models.PositiveIntegerField()
+    currentBid = models.ForeignKey("Bid", blank=True)
+    category = models.CharField(choices=CATEGORIES, blank=True)
+    image = models.URLField(blank=True)
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=255)
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
