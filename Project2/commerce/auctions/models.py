2f9cf330b56260b9341c44d8947a341ac17a778d
Varun Rao <varunvellorerao@gmail.com>
2024-02-03 14:39:24 -0500

    More changes to models

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -20,8 +20,8 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField(max_length=255)
     startingBid = models.PositiveIntegerField()
-    currentBid = models.ForeignKey("Bid", on_delete=models.CASCADE, blank=True)
-    category = models.CharField(choices=CATEGORIES, blank=True)
+    currentBid = models.ForeignKey("Bid", on_delete=models.CASCADE, blank=True, related_name="currentBid")
+    category = models.CharField(max_length=64, choices=CATEGORIES, blank=True)
     image = models.URLField(blank=True)
 
 class Comment(models.Model):
@@ -30,6 +30,7 @@ class Comment(models.Model):
     comment = models.TextField(max_length=255)
 
 class Bid(models.Model):
+    bidID = models.AutoField(primary_key=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     bidder = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.PositiveIntegerField()
