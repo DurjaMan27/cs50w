340bcffe0a2187399d9063f737fe81191734b564
Varun Rao <varunvellorerao@gmail.com>
2024-02-04 17:34:20 -0500

    Correcting the link to include listingID attribute

--- Project2/commerce/auctions/urls.py
+++ Project2/commerce/auctions/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path("listing/<str:username>/<int:listingID>", views.listing, name="listing"),
     path("addWatchList/<int:listingID>", views.addWatchList, name="addWatchList"),
     path("watchlist", views.watchList, name="watchlist"),
-    path("closeListing", views.closeListing, name="closeListing")
+    path("closeListing/<int:listingID>", views.closeListing, name="closeListing")
 ]
